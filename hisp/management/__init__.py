d77c47a3e7bf1d6d570cced98a6e190c2c5bae41
Nate <nate@natesoares.com>
2010-12-11 21:00:05 -0500

    Added HISP_FILETYPE setting

--- hisp/management/__init__.py
+++ hisp/management/__init__.py
@@ -1,11 +1,18 @@
 from django.conf import settings
-from hisp.doctypes import DJANGO
+from django.core.excepctions import ImproperlyConfigured
+from hisp.doctypes import DJANGO, FILETYPES
 from hisp import Hisp
 
 
 def hisper(filetype=None, debug=False, libraries=None):
     if filetype is None and 'django_html' in settings.INSTALLED_APPS:
         filetype = DJANGO
+    if filetype is None:
+        filetype = settings.get('HISP_FILETYPE', None)
+        filetype = filetype and filetype.upper()
+        if filetype not in FILETYPES:
+            raise ImproperlyConfigured("HISP_FILETYPE must be one of "
+                "'html', 'xhtml', 'django', or None")
     if libraries is None:
         libraries = settings.get('HISP_LIBRARIES', (
             'hisp.libraries.django.macros',
