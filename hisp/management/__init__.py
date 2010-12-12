fb25a363ee3a4af53e6feb73cb6705b280baa55d
Nate <nate@natesoares.com>
2010-12-12 18:22:46 -0500

    fixed minor typos

--- hisp/management/__init__.py
+++ hisp/management/__init__.py
@@ -7,12 +7,12 @@ from hisp import Hisp
 def hisper(filetype=None, debug=False, libraries=None):
     if filetype is None and 'django_html' in settings.INSTALLED_APPS:
         filetype = DJANGO
-    if filetype is None:
+    elif filetype is None:
         filetype = getattr(settings, 'HISP_FILETYPE', None)
         filetype = filetype and filetype.upper()
         if filetype not in FILETYPES:
             raise ImproperlyConfigured("HISP_FILETYPE must be one of "
-                "'html', 'xhtml', 'django', or None")
+                "'html', 'xhtml', 'django', or None.")
     if libraries is None:
         libraries = getattr(settings, 'HISP_LIBRARIES', (
             'hisp.libraries.django.macros',
