87ec4510f35edcd66e4e5bcb65d8609d3647368d
Nate <nate@natesoares.com>
2010-12-15 12:39:19 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
 	name='hisp',
-	version='1.0.0a',
+	version='1.1.0a',
 	install_requires=['ply==3.3'],
     description='HTML generator. Lisp syntax. Django integration.',
 	author='Nate Soares',
 	author_email='nate@natesoares.com',
     license='MIT',
-	# url='http://hisp.natesoares.com',
+    url='https://github.com/Soares/hisp',
 	packages=[
         'hisp', 'hisp.libraries', 'hisp.tables',
         'hisp.loaders', 'hisp.management', 'hisp.management.commands',
